cfc009cf0a619f5caabb48a91bb692e0252b3023
Govind Kumar <govindkumar.gpj@gmail.com>
2026-08-05 22:30:24 +0530

    feat: add caffeine cache, swagger openapi dependencies and config classes

--- weather-app/src/main/java/com/example/weather_app/WeatherAppApplication.java
+++ weather-app/src/main/java/com/example/weather_app/WeatherAppApplication.java
@@ -1,13 +1,13 @@
-package com.example.weather_app;
+package com.example.weatherapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class WeatherAppApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(WeatherAppApplication.class, args);
 	}
-
 }
